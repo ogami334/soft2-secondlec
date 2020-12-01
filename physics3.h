e28d2b48aa78c1a24ec813496bda2abd9d213ce8
ogami <ogami-takuya40439@g.ecc.u-tokyo.ac.jp>
2020-12-01 14:48:14 +0900

    update

--- physics3.h
+++ physics3.h
@@ -13,42 +13,51 @@ typedef struct condition
 typedef struct object
 {
   double m;
+  double x;
   double y;
+  double prev_x;
   double prev_y; // 壁からの反発に使用
+  double vx;
   double vy;
 } Object;
 
 // 授業で用意した関数のプロトタイプ宣言
 
-//void plot_objects(Object objs[], const size_t numobj, const double t, const Condition cond);
-//void update_velocities(Object objs[], const size_t numobj, const Condition cond);
-//void update_positions(Object objs[], const size_t numobj, const Condition cond);
-//void bounce(Object objs[], const size_t numobj, const Condition cond);
+void plot_objects(Object objs[], const size_t numobj, const double t, const Condition cond);
+void update_velocities(Object objs[], const size_t numobj, const Condition cond);
+void update_positions(Object objs[], const size_t numobj, const Condition cond);
+void bounce(Object objs[], const size_t numobj, const Condition cond);
 
 // To do: 以下に上記と同じ引数で実行される my_* を作成
 // 実装できたらmain関数で上記と入れ替えていく
 // my_plot_objects(), my_update_velocities(), my_update_positions(), my_bounce の4つ 
 void my_update_velocities_and_positions(Object objs[], const size_t numobj, const Condition cond) {
-  double accels[numobj];
+  double accels[numobj][2];
   for (int i=0;i<numobj;i++) {
-    accels[i]=0;
-  }
+    for (int j=0;j<2;j++) {
+        accels[i][j]=0;        
+    }
+  } //j=0がx
   for (int i=0;i<numobj;i++) {
     objs[i].prev_y=objs[i].y;
+    objs[i].prev_x=objs[i].x;
   }
   for (int i=0;i<numobj;i++) {
     for (int j=0;j<numobj;j++) {
       if (i==j) {
         continue;
       }
-      accels[i]+=(cond.G)*(objs[j].m)/pow(fabs(objs[j].y-objs[i].y),3)*(objs[j].y-objs[i].y);
+      accels[i][0]+=(cond.G)*(objs[j].m)/pow(pow(objs[j].y-objs[i].y,2)+pow(objs[j].x-objs[i].x,2),1.5)*(objs[j].x-objs[i].x);
+      accels[i][1]+=(cond.G)*(objs[j].m)/pow(pow(objs[j].y-objs[i].y,2)+pow(objs[j].x-objs[i].x,2),1.5)*(objs[j].y-objs[i].y);
     }
   }//位置を更新する前に加速度を出しておく
   for (int i=0;i<numobj;i++) {
+    objs[i].x=objs[i].x+objs[i].vx * cond.dt;
     objs[i].y=objs[i].y+objs[i].vy * cond.dt;
   } //速度を更新する前に位置を更新する
   for (int i=0;i<numobj;i++) {
-    objs[i].vy =objs[i].vy + accels[i] * cond.dt;
+    objs[i].vx =objs[i].vx + accels[i][0] * cond.dt;
+    objs[i].vy =objs[i].vy + accels[i][1] * cond.dt;
   } //最後に速度を更新
 }
 //void my_update_positions(Object objs[], const size_t numobj, const Condition cond);
@@ -62,8 +71,8 @@ void my_plot_objects(Object objs[], const size_t numobj, const double t, const C
     }
   }
   for (int i=0;i<numobj;i++) {
-    if ((-cond.height <= 2*objs[i].y) && (2*objs[i].y <=cond.height)) {
-      map[(int) (objs[i].y + cond.height/2)][cond.width- cond.width/2-1]=1;
+    if ((-cond.height <= 2*objs[i].y) && (2*objs[i].y <=cond.height) && (-cond.width <= 2*objs[i].x) && (2*objs[i].x <= cond.width)) {
+      map[(int) (objs[i].y + cond.height/2)][(int) (objs[i].x + cond.width/2)]=1;
     }
   }
   for (int i=0;i<cond.width+2;i++) {
@@ -88,6 +97,7 @@ void my_plot_objects(Object objs[], const size_t numobj, const double t, const C
   printf("\n");
   printf("t = %.1lf ",t);
   for (int i=0;i<numobj;i++) {
+    printf("objs[%d].x = %.2lf ",i,objs[i].x);
     printf("objs[%d].y = %.2lf ",i,objs[i].y);
   }
   printf("\n");
@@ -96,14 +106,23 @@ void my_plot_objects(Object objs[], const size_t numobj, const double t, const C
 
 void my_bounce(Object objs[], const size_t numobj, const Condition cond) {
   for (int i=0;i<numobj;i++) {
-    if (objs[i].prev_y<cond.height/2 && objs[i].y>cond.height/2) {
-      objs[i].vy*= -(cond.cor);
+    if (objs[i].prev_y < cond.height/2 && objs[i].y > cond.height/2) {
+      objs[i].vy *= -(cond.cor);
       objs[i].y = cond.height -objs[i].y;
     }
-    else if (objs[i].prev_y>-cond.height/2 && objs[i].y<-cond.height/2) {
+    else if (objs[i].prev_y > -cond.height/2 && objs[i].y < -cond.height/2) {
       objs[i].vy *= -(cond.cor);
       objs[i].y = -cond.height - objs[i].y;
     }
+    if (objs[i].prev_x < cond.width/2 && objs[i].x > cond.width/2) {
+      objs[i].vx*= -(cond.cor);
+      objs[i].x = cond.width -objs[i].x;
+    }
+    else if (objs[i].prev_x > -cond.width/2 && objs[i].x < -cond.width/2) {
+      objs[i].vx *= -(cond.cor);
+      objs[i].x = -cond.width - objs[i].x;
+    }
+
   }
 } //衝突に関してはひとまず、衝突せずに動いた場合の壁からの距離を折り返し、速度を(-e)倍にするという実装にしている
 //速度がとても大きいとバグの原因になりそう
